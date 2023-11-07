9023ffdb61d9fce98d766971f9d53ce120a553de
Patri42 <patrick.borzych@gmail.com>
2023-11-07 10:52:44 +0100

    update

--- example/main.cpp
+++ example/main.cpp
@@ -8,17 +8,16 @@ using namespace AML;
 
 int main (int argc, char **argv){
 
-    Vector3 v1(1.0, 2.0, 3.0);
-    Vector3 v2(4.0, 5.0, 6.0);
+    double data [3][3] = {{1.2, 3.2, 0.5}, {0.1, 1.0, 6.0}, {-4.1, 6.0, 8.0}};
+    Matrix33 Mat1 = Matrix33 (data);
 
-    Vector3 v3 = v1 + v2;
+    Vector3 v1 = Vector3 (1,0,0);
 
-    Vector3 v4 = v3 * v1;
+    Vector3 v2 = Mat1 * v1; 
 
     std::cout << v1 << std::endl;
+    std::cout << Mat1 << std::endl;
     std::cout << v2 << std::endl;
-    std::cout << v3 << std::endl;
-    std::cout << v4 << std::endl;
 
     return 0;
 }
