868f10926c8bc0e3d2708630bb5c5159d63e31d9
Patri42 <patrick.borzych@gmail.com>
2023-11-07 10:12:24 +0100

    update

--- AML/AMLVector3.cpp
+++ AML/AMLVector3.cpp
@@ -2,9 +2,7 @@
 #include <cmath>
 
 namespace AML
-
 {
-    
     // Constructors
     Vector3::Vector3()
     :x(0.0), y(0.0), z(0.0)
@@ -87,9 +85,9 @@ namespace AML
     }
 
     // Special Object Creators
-    static const Vector3 xAxis(){return Vector3(1.0, 0.0, 0.0);} //removed ::Vector3
-    static const Vector3 yAxis(){return Vector3(0.0, 1.0, 0.0);}
-    static const Vector3 zAxis(){return Vector3(0.0, 0.0, 1.0);}
+    const Vector3 Vector3::xAxis() {return Vector3(1.0, 0.0, 0.0);}
+    const Vector3 Vector3::yAxis() {return Vector3(0.0, 1.0, 0.0);}
+    const Vector3 Vector3::zAxis() {return Vector3(0.0, 0.0, 1.0);}
 
     // Vector / Vector Elementwise Operations
     Vector3 operator- (const Vector3& rhs){return Vector3(-rhs.x, -rhs.y, -rhs.z);}
--- example/main.cpp
+++ example/main.cpp
@@ -8,8 +8,17 @@ using namespace AML;
 
 int main (int argc, char **argv){
 
-    Vector3 
+    Vector3 v1(1.0, 2.0, 3.0);
+    Vector3 v2(4.0, 5.0, 6.0);
 
+    Vector3 v3 = v1 + v2;
+
+    Vector3 v4 = v3 * v1;
+
+    std::cout << v1 << std::endl;
+    std::cout << v2 << std::endl;
+    std::cout << v3 << std::endl;
+    std::cout << v4 << std::endl;
 
     return 0;
 }
