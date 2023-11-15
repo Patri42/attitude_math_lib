3176a3b0ee4e2d4d9db98134d371c12df03938f6
Patri42 <patrick.borzych@gmail.com>
2023-11-15 12:03:09 +0100

    update

--- AML/AMLDCM.h
+++ AML/AMLDCM.h
@@ -14,17 +14,16 @@ namespace AML
             static const Matrix33 rotationX(double theta);
             static const Matrix33 rotationY(double theta);
             static const Matrix33 rotationZ(double theta);
-
     };
 
     // DCM Functions
     bool isValidDCM(const Matrix33& dcm, double tol = std::numeric_limits<double>::epsilon());
-    void normaliseDCM(Matrix& dcm);
+    void normaliseDCM(Matrix33& dcm);
 
     // DCM Kinematic Functions
     Matrix33 integrateDCM(const Matrix33& dcm, const Matrix33& dcmRates, double dt);
     Matrix33 dcmKinematicRates_BodyRates (const Matrix33& dcm, const Vector3& bodyRates);
-    Matrix33 dcmKinematicRates_WorldRates (const Matrix33& dcm, const Vector3& bodyRates);
+    Matrix33 dcmKinematicRates_WorldRates (const Matrix33& dcm, const Vector3& worldRates);
 
 
 };
